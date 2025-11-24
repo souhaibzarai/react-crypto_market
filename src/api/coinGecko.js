da7773825eaad13dafd1abf0cb64830902da0639
souhaibzarai <souhaibzarai18@gmail.com>
2025-11-24 10:32:56 +0100

    logic to fetch coin data

--- src/api/coinGecko.js
+++ src/api/coinGecko.js
@@ -1,11 +1,20 @@
 const BASE_URL = 'https://api.coingecko.com/api/v3';
 
 export const fetchCryptos = async () => {
-  const response = await fetch(`${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250`)
+  const response = await fetch(`${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20`)
 
   if (!response.ok) {
     throw new Error('Failed to fetch cryptos');
   }
 
+  return response.json();
+}
+
+export const fetchCoinDataById = async (id) => {
+  const response = await fetch(`${BASE_URL}/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch coin data');
+  }
   return response.json();
 }
